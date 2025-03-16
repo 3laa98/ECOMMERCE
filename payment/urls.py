fe094ce6a5201c20fbe3ad300c950ce09c453b27
Alaa <alaa98.ash@gmail.com>
2025-03-17 02:19:24 +0300

    add payment model

---
+++ payment/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from payment import views
+
+urlpatterns = [
+    path('create-payment-intent/', views.create_payment_intent, name='create-payment-intent'),
+    path('success/', views.success, name='success'),
+    path('cancel/', views.cancel, name='cancel'),
+]
--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
-
+import stripe
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from cart.models import Cart
+from order.views import create_order
 # Create your views here.
+
+@csrf_exempt
+def create_payment_intent(request):
+    if request.method == "POST":
+        try:
+            # Get cart total from frontend
+            data = json.loads(request.body)
+            cart = Cart.objects.get(user=request.user, is_active=True)
+            total_amount = int(cart.get_cart_total() * 100)  # Convert to cents
+
+            # Create a PaymentIntent
+            intent = stripe.PaymentIntent.create(
+                amount=total_amount,
+                currency="usd",
+                payment_method_types=["card"]
+            )
+
+            order = create_order(request.user, cart)
+
+            return JsonResponse({"clientSecret": intent.client_secret})
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
+def success(request):
+    """Render success page after payment."""
+    return render(request, "order/success.html")
+
+def cancel(request):
+    """Render cancel page if payment is canceled."""
+    return render(request, "order/cancel.html")
