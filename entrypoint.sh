1ae7d5e8bc9779f7b195e9a96fb5beb473e58225
Alaa <alaa98.ash@gmail.com>
2025-04-09 23:59:11 +0300

    exec gunicorn instead of runserver

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,7 @@ set -e
 
 python manage.py migrate
 python manage.py collectstatic --noinput
-exec python manage.py runserver 0.0.0.0:8000
+exec gunicorn app.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --workers 3 \
+    --timeout 60
